9d019202c1f9d257d0711403bb84521b843d7bc1
christianokuru <okuruchristian@gmail.com>
2024-10-21 11:00:19 +0100

    Improved the form UI

--- src/components/Formss.vue
+++ src/components/Formss.vue
@@ -4,29 +4,32 @@ import { EyeIcon } from "@heroicons/vue/solid";
 
 <template>
   
-  <div class="bg-white px-8 pt-11 w-80 mx-auto">
+  <div class="bg-white px-12 pt-9 w-80 mx-auto">
     <form>
+      <!-- Username field -->
       <div class="mb-4">
         <label class="block text-sm font-semibold mb-2">Username</label>
         <input
           type="text"
-          class="w-full border border-gray-300 rounded-lg p-2"
+          class="w-full border border-gray-500 rounded-lg p-2"
         />
       </div>
-      <div class="mb-1">
+      <!-- Password field -->
+      <div class="mb-0 mt-[-7px]">
         <label class="block text-sm font-semibold mb-2">Password</label>
         <div class="relative">
           <input
             type="password"
-            class="w-full border border-gray-300 rounded-lg p-2"
+            class="w-full border border-gray-500 rounded-lg p-2"
           />
           <EyeIcon
             class="absolute right-3 top-3 w-5 h-5 text-gray-500 cursor-pointer"
           />
         </div>
       </div>
-      <div class="text-right mb-7">
-        <span class="text-sm">Forgot </span>
+      <!-- Sign in Button -->
+      <div class="text-right mb-6">
+        <span class="text-sm ">Forgot </span>
         <a href="#" class="text-blue-500 text-sm underline"
           >username</a
         >
