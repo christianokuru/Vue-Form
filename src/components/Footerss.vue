049ae6af046f47d3cff943b8935b8b62da1bb833
christianokuru <okuruchristian@gmail.com>
2024-10-21 11:25:05 +0100

    Created Mobile responsive Footer

--- src/components/Footerss.vue
+++ src/components/Footerss.vue
@@ -1,15 +1,39 @@
 <script setup></script>
 
+
 <template>
-  <footer class="mt-10 text-center text-xs text-gray-500">
-    <div class="space-y-1">
-      <a href="#" class="hover:underline">Terms of Service</a>
-      <a href="#" class="hover:underline">Notice of Privacy Practices</a>
-      <a href="#" class="hover:underline"
-        >Notice of Non-Discrimination and Language Assistance</a
-      >
-      <a href="#" class="hover:underline">Informed Consent Agreement</a>
-    </div>
-    <p class="mt-2">© 2002-2024 Teladoc, Inc. All rights reserved.</p>
-  </footer>
+  <div :class="['lg:flex-row lg:items-center mt-8 mx-5']">
+    <!-- Footer container -->
+    <footer :class="['flex flex-col items-center text-center text-[#5b2f91] font-semibold text-xs border-t border-dotted pt-4 border-gray-700 lg:flex-row lg:justify-between lg:items-center lg:py-6 lg:container lg:mx-auto']">
+      
+      <!-- Copyright for large screens (left) -->
+      <div :class="['hidden lg:block text-left text-gray-500 lg:w-1/5 font-extralight lg:flex-row lg:space-x-3 items-center lg:pb-3 text-xs']">
+        <p :class="['mt-2 lg:whitespace-nowrap']">© 2002-2024 Teladoc, Inc. All rights reserved.</p>
+      </div>
+
+      <!-- Links section -->
+      <div :class="['flex flex-col items-center space-y-2 lg:flex-row lg:space-y-0 lg:space-x-6 lg:w-2/3 lg:justify-end px-1']">
+        <router-link to="#">
+          <p :class="['hover:text-[#a77bca] pb-1 whitespace-nowrap']">Terms of Service</p>
+        </router-link>
+        <router-link to="#">
+          <p :class="['hover:text-[#a77bca] pb-1 whitespace-nowrap']">Notice of Privacy Practices</p>
+        </router-link>
+        <router-link to="#">
+          <p :class="['hover:text-[#a77bca] pb-1 break-words lg:whitespace-nowrap']">
+            Notice of Non-Discrimination and Language Assistance
+          </p>
+        </router-link>
+        <router-link to="#">
+          <p :class="['hover:text-[#a77bca] pb-1 whitespace-nowrap']">Informed Consent Agreement</p>
+        </router-link>
+      </div>
+
+      <!-- Copyright for small screens -->
+      <div :class="['text-center text-gray-500 lg:hidden mt-4 font-thin']">
+        <p :class="['mt-2 mb-28']">© 2002-2024 Teladoc, Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  </div>
 </template>
+
